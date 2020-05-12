69b960d23c48092750ae14776c1c6c9258d281b9
KazuichiroTaira <ktaira098@gmail.com>
2020-05-12 18:29:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# StandardDeviationAndMeans_DogsAtNursingHome
+ 
---
+++ StandardDiviationMean.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data = pd.read_csv('data/crowley-1996.csv')
+
+print(data)
+
+
