5f8118bc35fc76d0a5bfff6f3f5ea683b7825ad2
KazuichiroTaira <ktaira098@gmail.com>
2020-05-13 17:40:02 +0300

    update

--- StandardDeviationMean.py
+++ StandardDeviationMean.py
@@ -2,8 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-data = pd.read_csv('data/crowley-1996.csv')
+df = pd.read_csv('data/crowley-1996.csv')
 
-print(data)
-
+df[]
 
