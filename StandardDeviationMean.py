df42659b6da81cfec54249876f0eccbc21702ac9
KazuichiroTaira <ktaira098@gmail.com>
2020-05-15 19:25:57 +0300

    code is fixed but fig needed to be separated for the first two figs

--- StandardDeviationMean.py
+++ StandardDeviationMean.py
@@ -1,8 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
 
-df = pd.read_csv('data/crowley-1996.csv')
+DATA_FOLDER = 'data'
+FIG_FOLDER = 'fig'
+
+os.makedirs(FIG_FOLDER, exist_ok=True)
+
+data_file = os.path.join(DATA_FOLDER, 'crowley-1996.csv')
+df = pd.read_csv(data_file)
 
 # Number of people 'low' for the month 1, month 2, etc. for visiting
 
@@ -19,11 +26,12 @@ index = list(range(1, n_months + 1))
 
 for ds in dog_status:
 
-    mood_sums = {}
-    mood_mean = {}
-    for mood_label in moods.keys():
-        mood_sums[mood_label] = []
-        mood_mean[mood_label] = []
+    data = {}
+    labels = ['low', 'moderate', 'high', 'mean']
+    for label in labels:
+        data[label] = []
+
+    # data = {'low': [], 'moderate': [], 'high': [], 'mean':[]}
 
     is_ds = df['condition'] == ds
 
@@ -42,32 +50,27 @@ for ds in dog_status:
         for (mood_label, mood_value) in moods.items():
 
             is_mood = df[key_month] == mood_value
-            sum_mood = sum(df[is_mood & is_ds][key_month])
+            count_mood = len(df[is_mood & is_ds][key_month])
 
-            mood_sums[mood_label].append(sum_mood)
+            data[mood_label].append(count_mood)
 
-            is_mood = df[key_month] == mood_value
-            mean_mood = np.mean(df[is_ds][key_month])
+        mean_mood = np.mean(df[is_ds][key_month])
 
-            mood_mean[mood_label].append(mean_mood)
+        data['mean'].append(mean_mood)
 
-            # if mood == 1:
-            #    low.append(sum_actual_mood)
-            # elif mood == 2:
-            #    moderate.append(sum_actual_mood)
-            # else:
-            #    high.append
+    if ds == 'resident':
+        condition_label = ds
+    else:
+        condition_label = 'visiting'
 
-            if ds == 'resident':
-                condition_label = ds
-            else:
-                condition_label = 'visiting'
+    title = f'Tension x {condition_label} dog'
 
-            title = f'Tension x {condition_label} dog'
+    df_fig = pd.DataFrame(data, index=index)
+    ax = df_fig.plot.bar(rot=0)
+    ax.set_xlabel('month')
+    ax.set_ylabel('frequency')
+    ax.set_title(title)
 
-            df_fig = pd.DataFrame(mood_sums, mood_mean, index=index)
-            ax = df_fig.plot.bar(rot=0)
-            ax.set_xlabel('month')
-            ax.set_ylabel('frequency')
-            ax.set_title(title)
-            plt.show()
+    fig_name = f'{condition_label}.pdf'
+    fig_file = os.path.join(FIG_FOLDER, fig_name)
+    plt.savefig(fig_file)
